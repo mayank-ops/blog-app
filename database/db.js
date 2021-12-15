d20e1f31a68208c25925b9a256f2a70f30a4edeb
mayank <mayanksingh202@gmail.com>
2021-12-15 18:52:19 +0530

    added Pusher configs for real time comments update

--- database/db.js
+++ database/db.js
@@ -1,11 +1,40 @@
 import mongoose from 'mongoose'
+import Pusher from 'pusher'
+
+const pusher = new Pusher({
+    appId: "1312053",
+    key: "83a9269d53efd71b9741",
+    secret: "3578d7e4b08f034898d2",
+    cluster: "ap2",
+    useTLS: true
+});
 
 const Connection = () =>
 {
     const db_link = "mongodb+srv://admin:bXuZqb4GWMxheQ1G@cluster0.i6emt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-    mongoose.connect(db_link, { useUnifiedTopology: true})
+    mongoose.connect(db_link, { useUnifiedTopology: true })
         .then(() => console.log('db connection done'))
         .catch((err) => console.log(err.message))
+
+    const db = mongoose.connection;
+    db.once('open', () =>
+    {
+        console.log('db connection open');
+        const changeStream = db.collection('comments').watch();
+        changeStream.on('change', (change) =>
+        {
+            if (change.operationType === 'insert') {
+                pusher.trigger('comments', 'newComment', {
+                    "change": change
+                })
+            }
+            else if(change.operationType === 'delete'){
+                pusher.trigger('comments', 'commentDeleted', {
+                    "change": change
+                })
+            }
+        })
+    })
 }
 
 export default Connection;
